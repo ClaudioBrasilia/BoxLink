d10b0148f1a29005e9234ee943f277e0a7cc4b26
ClaudioBrasilia <claudiobrasilia13@gmail.com>
2026-05-03 20:59:18 -0300

    Import React in Admin.tsx

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { 
   LayoutDashboard, Users, MapPin, Calendar, Megaphone, Plus, Settings, 
   ChevronRight, ChevronDown, Activity, Check, X, Shield, UserPlus, 
